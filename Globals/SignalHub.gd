3cf9df0c770d54a4d1bd88ba9091884c7ef87ed2
66025009 <150766111+66025009@users.noreply.github.com>
2025-08-17 13:12:32 +0700

    Explosion feat.

--- Globals/SignalHub.gd
+++ Globals/SignalHub.gd
@@ -3,7 +3,7 @@ extends Node
 
 signal on_player_hit(v: int)
 signal on_score_updated(v: int)
-
+signal on_create_explosion(pos: Vector2, anim_name: String)
 
 func emit_on_player_hit(v: int) -> void:
 	on_player_hit.emit(v)
@@ -11,3 +11,7 @@ func emit_on_player_hit(v: int) -> void:
 
 func emit_on_score_updated(v: int):
 	on_score_updated.emit(v)
+
+
+func emit_on_create_explosion(pos: Vector2, anim_name: String):
+	on_create_explosion.emit(pos, anim_name)
