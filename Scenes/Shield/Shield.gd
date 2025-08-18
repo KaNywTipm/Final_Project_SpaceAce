2a3ca8a6ff76141ff1c42824ab6775dc6ed1adcf
McxJin <150765797+McxJin@users.noreply.github.com>
2025-08-18 13:34:27 +0700

    Update Shield

--- Scenes/Shield/Shield.gd
+++ Scenes/Shield/Shield.gd
@@ -17,4 +17,34 @@ var _health: int = start_health
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	disable_shield()
+
+
+func enable_shield() -> void:
+	animation_player.play("RESET")
+	_health = start_health
+	timer.start()
+	show()
+	SpaceUtils.toggle_area2d(self, true)
+	sound.play()
+
+
+func disable_shield() -> void:
+	timer.stop()
+	hide()
+	SpaceUtils.toggle_area2d(self, false)
+
+
+func hit() -> void:
+	animation_player.play("hit")
+	_health -= 1
+	if _health <= 0:
+		disable_shield()
+
+
+func _on_timer_timeout() -> void:
+	disable_shield()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	hit()
