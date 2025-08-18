5863517cfb6fa6ba6b61af6432d8e9cbf2851c97
McxJin <150765797+McxJin@users.noreply.github.com>
2025-08-18 16:23:44 +0700

    Player movement

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -8,10 +8,12 @@ const GROUP_NAME: String = "Player"
 
 
 @export var health_boost: int = 25
+@export var speed: float = 250.0
 
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var shield: Shield = $Shield
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -25,7 +27,25 @@ func _enter_tree() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var input = get_input()
+	global_position += input * delta * speed
+
+
+func get_input() -> Vector2:	
+	
+	var v = Vector2(
+		Input.get_axis("left", "right"),
+		Input.get_axis("up", "down")
+		)
+	
+	if v.x != 0:
+		animation_player.play("turn")
+		sprite_2d.flip_h = true if v.x > 0 else false
+	else:
+		animation_player.play("fly")
+		
+	return v.normalized()
+
 
 
 func _on_area_entered(area: Area2D) -> void:
